1816e492036fc51c9e5b7597fb96819667971467
Douglas Moura <douglas12345.dsm@gmail.com>
2025-02-04 16:26:37 -0300

    style: Retornando para o Tailwind V3
    - desinstalei o tailwind v4 e instalei o v3 mesmo
    - estrutura do formulário criado
    - estilo básico do formulário definido
    - fontfamily definida em Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     content: [
-        "./index.html",
+        "./src/index.html",
         "./src/**/*.{js,ts,jsx,tsx,vue}"
     ],
     theme: {
         extend: {},
+        fontFamily: {
+            'poppins': ['Poppins', 'sans-serif']
+        }
     },
     plugins: [],
 };
